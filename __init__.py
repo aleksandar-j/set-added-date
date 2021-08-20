15cc679d65c106729a026732a5a540561f4bd70b
aleksandar-j <aleksandarjasovic@gmail.com>
2021-08-20 14:55:53 +0200

    Rename variable to fit addon name 'edit'->'add'

--- __init__.py
+++ __init__.py
@@ -6,9 +6,9 @@ from aqt.qt import QAction
 from . import set_added_date
 
 def setupAction(browser):
-    actionEditAddedDate = QAction("Set Added Date...", browser)
-    actionEditAddedDate.triggered.connect(lambda: set_added_date.setAddedDate(browser))
+    actionSetAddedDate = QAction("Set Added Date...", browser)
+    actionSetAddedDate.triggered.connect(lambda: set_added_date.setAddedDate(browser))
 
-    browser.form.menu_Cards.insertAction(browser.form.menu_Cards.actions()[2], actionEditAddedDate)
+    browser.form.menu_Cards.insertAction(browser.form.menu_Cards.actions()[2], actionSetAddedDate)
 
 gui_hooks.browser_menus_did_init.append(setupAction)
